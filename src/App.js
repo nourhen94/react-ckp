1213b34255377f2c638baa8b2a66d4fb61a10668
nourhen94 <nourhen.jallouli@gmail.com>
2021-11-17 09:26:10 +0100

    1st

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Formbt from './component/Formbt';
+import Navbarbt from './component/Navbarbt';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div  className="App">
+      <header style={{backgroundColor:"blue"}} className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{fontSize: 100 }}>
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          welcome 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Navbarbt />
+      <Formbt />
       </header>
     </div>
   );
